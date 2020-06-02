c3d33b64c7f16a1e0e734f8d67aaed8a8e329f49
Richard Cho <96richardohc@gmail.com>
2020-06-02 07:19:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,10 +4,10 @@ class Backer
   def initialize(name)
     @name = name 
     @backed_projects = []
-    @backer =[]
   end
   
   def back_project(project)
     @backed_projects << project
+    
   end
 end
